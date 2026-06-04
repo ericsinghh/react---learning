8956adb885bd9996fc86a8f2de8f6ecf6424f459
ericsinghh <singheric890@gmail.com>
2026-06-05 01:49:52 +0530

    add image and adjust styling for overflow

--- src/App.jsx
+++ src/App.jsx
@@ -6,13 +6,12 @@ function App() {
 
   return (
     <>
-      <Navbar/>
+      <Navbar />
       <div className="cards">
       <Card title="Card 1" description="card 1 desc" />
       <Card title="Card 2" description="card 2 desc" />
       <Card title="Card 3" description="card 3 desc" />
       <Card title="Card 4" description="card 4 desc" />
-      
       </div>
       <Footer/>
     </>
